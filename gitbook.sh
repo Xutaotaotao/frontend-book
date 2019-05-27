bd6639f338fd51fbf3ec7bae376d7ac6efab843f
徐涛焘 <xutaotao1108@outlook.com>
2019-05-27 17:15:19 +0800

    更新说明

--- gitbook.sh
+++ gitbook.sh
@@ -1,8 +1,8 @@
 git add .
 git commit -m $1
 git push -u origin master
-git checkout pages
+git checkout gh-pages
 cp -r _book/* .
 git add .
 git commit -m $1
-git push -u origin pages
+git push -u origin gh-pages
